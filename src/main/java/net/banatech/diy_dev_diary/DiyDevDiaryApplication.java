9f7f6f25f5eb8f5d2464ff66198b4a377b1b6261
bana118 <bana.titech@gmail.com>
2022-02-10 10:27:57 +0000

    add spring boot init program

---
+++ src/main/java/net/banatech/diy_dev_diary/DiyDevDiaryApplication.java
@@ -0,0 +1,13 @@
+package net.banatech.diy_dev_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyDevDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiyDevDiaryApplication.class, args);
+	}
+
+}
