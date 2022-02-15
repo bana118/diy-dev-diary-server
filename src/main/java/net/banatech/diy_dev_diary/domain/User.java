d6b5720c1f41d913020591e7adad7d65ca2ee770
bana118 <bana.titech@gmail.com>
2022-02-15 19:02:34 +0900

    fix database settings

--- src/main/java/net/banatech/diy_dev_diary/domain/User.java
+++ src/main/java/net/banatech/diy_dev_diary/domain/User.java
@@ -4,11 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Data
 @NoArgsConstructor
 public class User {
