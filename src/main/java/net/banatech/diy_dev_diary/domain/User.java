4ed441b34a3e0e35fb55a38033ad8e7f38273e24
bana118 <bana.titech@gmail.com>
2022-02-15 15:35:48 +0900

    add postgresql setting

--- src/main/java/net/banatech/diy_dev_diary/domain/User.java
+++ src/main/java/net/banatech/diy_dev_diary/domain/User.java
@@ -1,11 +1,19 @@
 package net.banatech.diy_dev_diary.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
     private String mailAddress;
