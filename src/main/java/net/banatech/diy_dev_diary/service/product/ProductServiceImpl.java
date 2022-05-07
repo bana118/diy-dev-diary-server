982a37dfd117f4d6e7f45f2fa7a15fafda552a60
bana118 <bana.titech@gmail.com>
2022-05-07 21:37:56 +0900

    add empty service files

---
+++ src/main/java/net/banatech/diy_dev_diary/service/product/ProductServiceImpl.java
@@ -0,0 +1,5 @@
+package net.banatech.diy_dev_diary.service.product;
+
+public class ProductServiceImpl implements ProductService {
+
+}
---
+++ src/main/java/net/banatech/diy_dev_diary/service/user/UserServiceImpl.java
@@ -0,0 +1,5 @@
+package net.banatech.diy_dev_diary.service.user;
+
+public class UserServiceImpl implements UserService {
+
+}
