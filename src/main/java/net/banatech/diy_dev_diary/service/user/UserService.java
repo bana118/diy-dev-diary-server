c14e12b20a85cd085b6627b6d028a9bc7a899250
bana118 <bana.titech@gmail.com>
2022-05-07 21:55:46 +0900

    impl UserServiceImpl

--- src/main/java/net/banatech/diy_dev_diary/service/user/UserService.java
+++ src/main/java/net/banatech/diy_dev_diary/service/user/UserService.java
@@ -1,5 +1,18 @@
 package net.banatech.diy_dev_diary.service.user;
 
-public interface UserService {
+import org.springframework.stereotype.Service;
 
+import net.banatech.diy_dev_diary.domain.User;
+
+@Service
+public interface UserService {
+    User findOne(String userId);
+
+    void insert(User user);
+
+    void update(User user);
+
+    void delete(User user);
+
+    void deleteAll();
 }
--- src/main/java/net/banatech/diy_dev_diary/service/user/UserServiceImpl.java
+++ src/main/java/net/banatech/diy_dev_diary/service/user/UserServiceImpl.java
@@ -1,5 +1,50 @@
 package net.banatech.diy_dev_diary.service.user;
 
+import org.springframework.transaction.annotation.Transactional;
+
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+
+import net.banatech.diy_dev_diary.domain.User;
+import net.banatech.diy_dev_diary.repository.user.UserRepository;
+
 public class UserServiceImpl implements UserService {
+    // private static final Logger logger =
+    // LoggerFactory.getLogger(UserServiceImpl.class);
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findOne(String userId) {
+        return this.userRepository.findOne(userId);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Throwable.class)
+    public void insert(User user) {
+        this.userRepository.insert(user);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Throwable.class)
+    public void update(User user) {
+        this.userRepository.update(user);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Throwable.class)
+    public void delete(User user) {
+        this.userRepository.delete(user);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Throwable.class)
+    public void deleteAll() {
+        this.userRepository.deleteAll();
+    }
 
 }
